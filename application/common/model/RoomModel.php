f7a1aac842a381969aff0ac82fe39b8913ab8488
MRXY001 <wxy19980615@gmail.com>
2020-03-03 22:31:45 +0800

    添加房间排行榜

---
+++ application/common/model/RoomModel.php
@@ -0,0 +1,8 @@
+<?php
+namespace app\common\model;
+use think\Model;
+
+class RoomModel extends Model
+{
+    protected $table = 'rooms';
+}
--- application/index/controller/IndexController.php
+++ application/index/controller/IndexController.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\index\controller;
 use app\common\model\UserModel;
+use app\common\model\RoomModel;
 use think\Controller;
 use think\Request;
 
@@ -16,19 +17,50 @@ class IndexController extends Controller
         $type = Request::instance()->param('type');
         $sort = $type;
         if (is_null($type) || $type == 'level')
+        {
+            $type = 'level';
             $sort = 'level desc';
+        }
         else if ($type == 'yestoday')
             $sort = 'words_yestoday desc';
         else if ($type == 'today')
             $sort = new \think\db\Expression('allwords - allwords_yestoday desc');
+        else if ($type == 'room' || $type == 'myroom')
+            $sort = 'level DESC';
         else
+        {
+            $type = 'level';
             $sort = 'level desc';
+        }
 
         if ($type != 'room' && $type != 'myroom')
         {
             $users = new UserModel();
             $time = time();
             $users->where("allwords>allwords_yestoday or words_yestoday>0 or VIP_deadline>0 or sync_time>$time")
+                  ->order($sort);
+            $users = $users->select();
+            
+            $this->assign('users', $users);
+            $this->assign('time', $time);
+            $this->assign('online_time', $time - 900);
+            return $this->fetch('rank');
+        }
+        else if ($type == 'room')
+        {
+            $rooms = new RoomModel();
+            $rooms->order($sort);
+            $rooms = $rooms->select();
+            
+            $this->assign('rooms', $rooms);
+            return $this->fetch('rooms');
+        }
+        else if ($type == 'myroom')
+        {
+            $roomID = Request::instance()->param('room_id');
+            $users = new UserModel();
+            $time = time();
+            $users->where("room_id = $roomID")
                   ->order($sort);
             $users = $users->select();
             
