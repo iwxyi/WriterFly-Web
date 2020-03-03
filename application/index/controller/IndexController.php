5a8489cf32bca7342d7e9a76a6f0787e9938dee5
MRXY001 <wxy19980615@gmail.com>
2020-03-03 17:37:57 +0800

    添加排行榜信息显示

--- application/index/controller/IndexController.php
+++ application/index/controller/IndexController.php
@@ -14,15 +14,25 @@ class IndexController extends Controller
     public function rank()
     {
         $type = Request::instance()->param('type');
-        if (is_null($type))
-            $type = 'level';
-        $users = new UserModel();
-        $users->where("allwords>allwords_yestoday or words_yestoday>0 or VIP_deadline>0")
-              ->order($type);
-        $users = $users->select();
-        
-        $this->assign('users', $users);
-        
-        return $this->fetch('rank');
+        $sort = $type;
+        if (is_null($type) || $type == 'level')
+            $sort = 'level desc';
+        else if ($type == 'yestoday')
+            $sort = 'words_yestoday desc';
+        else if ($type == 'today')
+            $sort = new \think\db\Expression('allwords - allwords_yestoday desc');
+        else
+            $sort = 'level desc';
+
+        if ($type != 'room' && $type != 'myroom')
+        {
+            $users = new UserModel();
+            $users->where("allwords>allwords_yestoday or words_yestoday>0 or VIP_deadline>0")
+                  ->order($sort);
+            $users = $users->select();
+            
+            $this->assign('users', $users);
+            return $this->fetch('rank');
+        }
     }
 }
