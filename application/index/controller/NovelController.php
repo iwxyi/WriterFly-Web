02e4f199b7bba3d56d0fea2b199c9a1ac1abd0e3
MRXY001 <wxy19980615@gmail.com>
2020-03-05 14:17:25 +0800

    添加发布界面

--- application/index/controller/NovelController.php
+++ application/index/controller/NovelController.php
@@ -45,7 +45,7 @@ class NovelController extends Controller
     {
         $novelname = Request::instance()->param('novelname');
         $chapters = new ChapterModel();
-        $chapters->where("userID='".session('user_id')."' and novelname='$novelname' and del=0");
+        $chapters->where("userID='".session('user_id')."' and novelname='$novelname' and kind=0 and del=0");
         $chapters->order('sync_time desc');
         return $this->fetch('dir');
     }
@@ -54,7 +54,7 @@ class NovelController extends Controller
     {
         $novelname = Request::instance()->param('novelname');
         $chapters = new ChapterModel();
-        $chapters->where("userID='".session('user_id')."' and novelname='$novelname' and del=0");
+        $chapters->where("userID='".session('user_id')."' and novelname='$novelname' and kind=0 and del=0");
         $chapters->order('sync_time desc');
         $chapters = $chapters->select();
         
@@ -66,7 +66,7 @@ class NovelController extends Controller
     public function chapter()
     {
         $chapterID = Request::instance()->param('chapter_id');
-        $chapter = ChapterModel::get(['chapterID' => $chapterID, 'userID' => session('user_id')]);
+        $chapter = ChapterModel::get(['chapterID' => $chapterID, 'userID' => session('user_id'), 'kind' => 0]);
         $this->assign('chapter', $chapter);
         return $this->fetch('chapter');
     }
@@ -77,16 +77,95 @@ class NovelController extends Controller
         $chapterID = $param['chapter_id'];
         $title = $param['title'];
         $body = $param['body'];
-        $chapter = ChapterModel::get(['chapterID' => $chapterID]);
+        $chapter = ChapterModel::get(['chapterID' => $chapterID, 'userID' => session('user_id')]);
         if (is_null($chapterID))
-            return $this->error('没有这个章节');
+            return $this->error('没有这篇章节');
         
         $chapter['title'] = $title;
         $chapter['body'] = $body;
         $chapter['alter_time'] = $chapter['sync_time'] = time();
         $chapter->validate()->save();
         
-        return 'save success';
+        return 'save succeed';
+    }
+    
+    public function goPublishChapter()
+    {
+        $param = Request::instance()->param();
+        $chapterID = $param['chapter_id'];
+        $chapter = ChapterModel::get(['chapterID' => $chapterID, 'userID' => session('user_id')]);
+        if (is_null($chapterID))
+            return $this->error('没有这篇章节');
+        
+        if ($chapter['publish_state'] == 0) // 直接发布
+        {
+            $chapter['publish_title'] = $chapter['title'];
+            $chapter['publish_body'] = $chapter['body'];
+            $chapter['publish_time'] = $chapter['sync_time'] = time();
+            $chapter['publish_state'] = 1;
+        }
+        $chapter->validate()->save();
+        
+        $this->assign('chapter', $chapter);
+        return $this->fetch('publishChapter');
+    }
+    
+    public function publishChapter()
+    {
+        $param = Request::instance()->param();
+        $chapterID = $param['chapter_id'];
+        $chapter = ChapterModel::get(['chapterID' => $chapterID, 'userID' => session('user_id')]);
+        if (is_null($chapterID))
+            return $this->error('没有这篇章节');
+        
+        if ($chapter['publish_state'] < 0) // 禁止
+        {
+            return 'banned';
+        }
+        
+        $chapter['publish_state'] = 1;
+        $chapter['publish_title'] = $chapter['title'];
+        $chapter['publish_body'] = $chapter['body'];
+        $chapter['publish_time'] = time();
+        $chapter->validate()->save();
+        return 'succeed';
+    }
+    
+    public function unpublishChapter()
+    {
+        $param = Request::instance()->param();
+        $chapterID = $param['chapter_id'];
+        $chapter = ChapterModel::get(['chapterID' => $chapterID, 'userID' => session('user_id')]);
+        if (is_null($chapterID))
+            return $this->error('没有这篇章节');
+        
+        if ($chapter['publish_state'] < 0) // 禁止
+        {
+            return 'banned';
+        }
+        
+        $chapter['publish_state'] = 0;
+        $chapter->validate()->save();
+        return 'succeed';
+    }
+    
+    public function livePublish()
+    {
+        $param = Request::instance()->param();
+        $chapterID = $param['chapter_id'];
+        $chapter = ChapterModel::get(['chapterID' => $chapterID, 'user_id' => session('user_id')]);
+        if (is_null($chapterID))
+            return $this->error('没有这篇章节');
+        
+        if ($chapter['live_publish'] == 1)
+            $chapter['live_publish'] = 0;
+        else
+        {
+            $chapter['live_publish'] = 1;
+            $chapter['publish_body'] = $chapter['body'];
+        }
+        $chapter->validate()->save();
+        return 'succeed';
     }
     
     public function outline()
