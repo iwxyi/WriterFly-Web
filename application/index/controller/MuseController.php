259ded70a0d6f9312c9d2286c407a097b52dfe58
MRXY001 <wxy19980615@gmail.com>
2020-03-09 23:05:20 +0800

    添加接力与举报

--- application/index/controller/MuseController.php
+++ application/index/controller/MuseController.php
@@ -27,6 +27,7 @@ class MuseController extends Controller
         $muses->where('floor = 1')->order('children_count desc, relay_time desc');
         $muses = $muses->paginate(30);
         $this->assign('muses', $muses);
+        $this->assign('offspring', true);
         return $this->fetch('list');
     }
     
@@ -36,7 +37,7 @@ class MuseController extends Controller
     public function latest()
     {
         $muses = new MuseModel();
-        $muses->order('relay_time desc');
+        $muses->order('create_time desc');
         $muses = $muses->paginate(30);
         $this->assign('muses', $muses);
         return $this->fetch('list');
@@ -47,6 +48,10 @@ class MuseController extends Controller
      */
     public function goCreate()
     {
+        $user = UserModel::currentUser();
+        $muse = MuseModel::get(['userID' => session('user_id'),'children_count'=>0]);
+        if (!is_null($muse))
+            return $this->error('您有情节未被接力，无法新建根情节');
         return $this->fetch('create');
     }
     
@@ -61,7 +66,6 @@ class MuseController extends Controller
             $this->assign('hasError', 'true');
             $this->assign('errorReason', '请输入30~300个汉字的内容');
             $this->assign('default', $content);
-            return $this->error($content);
             return $this->goCreate();
         }
         
@@ -101,7 +105,7 @@ class MuseController extends Controller
         
         // 获取子情节：create晚
         $children = new MuseModel();
-        $children->where("parentID='$museID' and create_time>'$create_time'")->order('create_time');
+        $children->where("parentID='$museID' and create_time>'$create_time'")->order('create_time desc');
         $children = $children->select();
         
         $this->assign('parents', $parents);
@@ -120,7 +124,7 @@ class MuseController extends Controller
         $muse = MuseModel::get(['museID' => $museID]);
         if (is_null($muse))
             return $this->error('未找到该情节');
-        if ($muse->userID ==  session('user_id'))
+        if ($muse->userID == session('user_id'))
             return $this->error('不能接力自己的情节');
     }
     
@@ -129,7 +133,37 @@ class MuseController extends Controller
      */
     public function relay()
     {
+        $parentID = Request::instance()->param('muse_id');
+        if (!isset($parentID) || $parentID == '')
+            return $this->error('未获取到情节');
+        $parent = MuseModel::get(['museID' => $parentID]);
+        if (is_null($parent))
+            return $this->error('未找到该情节');
+        /* if ($parent->userID == session('user_id'))
+            return $this->error('不能接力自己的情节'); */
+        $content = Request::instance()->param('content');
+        if (mb_strlen($content) >= 300 || mb_strlen($content) < 30)
+        {
+            return $this->error('请输入30~300字的汉字接力');
+        }
         
+        // 创建新的接力
+        $muse = new MuseModel();
+        $muse['userID'] = session('user_id');
+        $muse['parentID'] = $parentID;
+        $muse['content'] = $content;
+        $muse['floor'] = $parent['floor'] + 1;
+        $muse['relay_time'] = $muse['create_time'] = time();
+        $muse->validate()->save();
+        
+        $muse['path'] = $parent['path'] . $muse['museID'] . '/';
+        $muse->validate()->save();
+        
+        $parent['children_count'] = $parent['children_count'] + 1;
+        $parent['offspring_count'] = $parent['offspring_count'] + 1;
+        $parent['relay_time'] = time();
+        $parent->validate()->save();
+        return $this->success('接力成功', url('Muse/line?muse_id=' . $muse['museID']));
     }
     
     /**
@@ -139,5 +173,21 @@ class MuseController extends Controller
     public function mine()
     {
         
+    }
+    
+    /**
+     * 举报
+     */
+    public function report()
+    {
+        $museID = Request::instance()->param('muse_id');
+        if (!isset($museID) || $museID == '')
+            return $this->error('未获取到情节');
+        $muse = MuseModel::get(['museID' => $museID]);
+        if (is_null($muse))
+            return $this->error('未找到该情节');
+        $muse['report_count'] = $muse['report_count'] + 1;
+        $muse->validate()->save();
+        return $this->success('举报成功，请等待检查');
     }
 }
